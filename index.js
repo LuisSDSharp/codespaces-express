6c9f035757608d574f3ec8131365057d2f58538c
Luis Larghi <windinglair@gmail.com>
2023-06-07 13:56:20 +0000

    First commit

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const contentfulManagement = require('contentful-management');
+
 app.get('/', (req, res) => {
+  const client = contentfulManagement.createClient({
+    accessToken: 'CFPAT-FbO5rpGVCvOigcJOiZsbBwi6rjHNrLtlb7FsvNnqNMY',
+  });
+
+  client.getSpace('e48r2iwyjczp')
+    .then((space) => space.getEnvironment('staging'))
+    .then((env) => env.createEntry('localizedItem', {
+      fields: {
+        key: { 'en-US': 'example.from.express' },
+        content: { 'en-US': 'Example from Express' },
+      }
+    }))
+    .then((entry) => console.log('Entry created:', entry.sys.id))
+    .catch((error) => console.error('Error creating entry:', error));
+
   res.send('Hello World!')
 })
 
